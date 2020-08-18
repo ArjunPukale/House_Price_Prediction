5cc0fd93b25c9a808436acdb3aa9bc56f10f5fd3
Arjun Pukale <49120008+ArjunPukale@users.noreply.github.com>
2020-08-18 22:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,27 +23,27 @@ def predict():
         - name: House Age
           in: query
           type: number
-          description: "Enter age of the house"
+          description: "0 - 43"
           required: true
         - name: Distance_to_the_nearest_MRT_station
           in: query
           type: number
-          description: ""
+          description: "24 - 4k"
           required: true
         - name: number_of_convenience_stores
           in: query
           type: number
-          description: ""
+          description: "0-10"
           required: true
         - name: Latitude
           in: query
           type: number
-          description: ""
+          description: "24.93-25"
           required: true
         - name: Longitude
           in: query
           type: number
-          description: ""
+          description: "121.47 - 121.57"
           required: true
     responses:
           200:
